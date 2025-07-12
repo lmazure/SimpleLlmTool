50e8cb16326b01bdca32d9b8d2a8fe9715c15ec6
Laurent <laurent@gilgamesh>
2025-07-12 06:50:12 +0200

    feat: add Mistral AI provider

---
+++ src/main/java/fr/mazure/aitestcasegeneration/provider/mistralai/MistralAiChatModelProvider.java
@@ -0,0 +1,37 @@
+package fr.mazure.aitestcasegeneration.provider.mistralai;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.mistralai.MistralAiChatModel;
+import dev.langchain4j.model.mistralai.MistralAiChatModel.MistralAiChatModelBuilder;
+import fr.mazure.aitestcasegeneration.provider.base.MissingEnvironmentVariable;
+import fr.mazure.aitestcasegeneration.provider.base.ModelProvider;
+
+public class MistralAiChatModelProvider implements ModelProvider {
+    
+    public static ChatModel createChatModel(final MistralAiModelParameters parameters) throws MissingEnvironmentVariable {
+
+        final String apiKey = ModelProvider.getApiKeyFromEnvironmentVariable(parameters.getApiKeyEnvironmentVariableName(), "Mistral AI");
+
+        final MistralAiChatModelBuilder builder = MistralAiChatModel.builder()
+                                                                    .apiKey(apiKey)
+                                                                    .modelName(parameters.getModelName());
+
+        if (parameters.getUrl().isPresent()) {
+            builder.baseUrl(parameters.getUrl().get().toString());
+        }
+
+        if (parameters.getTemperature().isPresent()) {
+            builder.temperature(parameters.getTemperature().get());
+        }
+
+        if (parameters.getTopP().isPresent()) {
+            builder.topP(parameters.getTopP().get());
+        }
+
+        if (parameters.getMaxTokens().isPresent()) {
+            builder.maxTokens(parameters.getMaxTokens().get());
+        }
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/fr/mazure/aitestcasegeneration/provider/mistralai/MistralAiModelParameters.java
@@ -0,0 +1,78 @@
+package fr.mazure.aitestcasegeneration.provider.mistralai;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import org.yaml.snakeyaml.Yaml;
+
+import fr.mazure.aitestcasegeneration.provider.base.InvalidModelParameter;
+import fr.mazure.aitestcasegeneration.provider.base.MissingModelParameter;
+import fr.mazure.aitestcasegeneration.provider.base.ModelParameters;
+import fr.mazure.aitestcasegeneration.provider.base.ParameterMap;
+
+/**
+ * Parameters for the Mistral AI model provider.
+ * 
+ * @param modelName           the name of the model
+ * @param url                 the base URL of the provider
+ * @param apiKeyEnvVar        the name of the environment variable containing the API key
+ * @param temperature         the temperature of the model
+ * @param topP                the top P value of the model
+ * @param maxTokens           the maximum number of tokens the model should generate
+ */
+public class MistralAiModelParameters extends ModelParameters {
+    
+    private final Optional<Double> temperature;
+    private final Optional<Double> topP;
+    private final Optional<Integer> maxTokens;
+
+    public MistralAiModelParameters(final String modelName,
+                                    final Optional<URL> url,
+                                    final String apiKeyEnvVar,
+                                    final Optional<Double> temperature,
+                                    final Optional<Double> topP,
+                                    final Optional<Integer> maxTokens) {
+        super(modelName, url, apiKeyEnvVar);
+        this.temperature = temperature;
+        this.topP = topP;
+        this.maxTokens = maxTokens;
+    }
+
+    public Optional<Double> getTemperature() {
+        return this.temperature;
+    }
+
+    public Optional<Double> getTopP() {
+        return this.topP;
+    }
+
+    public Optional<Integer> getMaxTokens() {
+        return this.maxTokens;
+    }
+    
+    /**
+     * Load parameters from a YAML file and create a new MistralAiModelParameters instance.
+     * 
+     * @param yamlFilePath the path to the YAML file containing the parameters
+     * @return a new MistralAiModelParameters instance with the parameters from the YAML file
+     * @throws IOException if there is an error reading the file
+     * @throws MissingModelParameter if a compulsory parameter is missing
+     * @throws InvalidModelParameter if a parameter has an incorrect value
+     */
+    public static MistralAiModelParameters loadFromFile(final Path yamlFilePath) throws IOException, MissingModelParameter, InvalidModelParameter {
+        try (final InputStream inputStream = new FileInputStream(yamlFilePath.toFile())) {
+            final Yaml yaml = new Yaml();
+            final ParameterMap parameterMap = new ParameterMap(yaml.load(inputStream));
+            return new MistralAiModelParameters(parameterMap.getString("modelName"),
+                                                parameterMap.getOptionalUrl("url"),
+                                                parameterMap.getString("apiKeyEnvVar"),
+                                                parameterMap.getOptionalDouble("temperature"),
+                                                parameterMap.getOptionalDouble("topP"),
+                                                parameterMap.getOptionalInteger("maxTokens"));
+        }
+    }
+}
