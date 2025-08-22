784202c6b7c683acb6a9414c4e9bcb0ba53f7f1f
Laurent <laurent@gilgamesh>
2025-08-22 21:23:44 +0200

    feat: add date/time tool

---
+++ tools/get_date_and_time.py
@@ -0,0 +1,19 @@
+"""
+Script to return current date and time in ISO 8601 format (YYYY-MM-DDTHH:MM:SS.ffffff)
+"""
+
+import sys
+from datetime import datetime
+
+if __name__ == "__main__":
+    # Check command line arguments
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 2 and sys.argv[1] == "--description":
+            print("return the current date and time formated as YYYY-MM-DDTHH:MM:SS.ffffff")
+        else:
+            print("Error: Invalid argument. Only '--description' is allowed.", file=sys.stderr)
+            sys.exit(1)
+    else:
+        # Local time in ISO 8601 format
+        local_iso = datetime.now().isoformat()
+        print(local_iso)
